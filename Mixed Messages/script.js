50f898946f3db9a7e2589b6a2033689808684c2c
Origami Bear <mehmetcansavas@gmail.com>
2021-11-17 21:48:07 +0300

    Added script for the portfolio.

---
+++ Mixed Messages/script.js
@@ -0,0 +1,20 @@
+// Generate random index for selecting messages.
+const selectRandomIndex = (lengthOfArray) => Math.floor(Math.random() * lengthOfArray);
+
+// Arrays that build sentences.
+const subject = ['You', 'Your mother', 'Your love', 'Your family'];
+const timeHorizon = ['tomorrow', 'this year', 'next year', 'this month', 'next week', 'next month'];
+const topics = ['win a lottery', 'go to a trave', 'buy a house', 'get sick', 'buy a new car', 'find someone', 'get rich very quickly'];
+
+// Build sentence with a word from each array.
+const sentenceBuilder = () => {
+    const selectedSubject = subject[selectRandomIndex(subject.length)];
+    const selectedTimeHorizon = timeHorizon[selectRandomIndex(timeHorizon.length)];
+    const selectedTopic = topics[selectRandomIndex(topic.length)];
+
+    return `${selectedSubject} will ${selectedTopic} ${timeHorizon}, believe me dear!`;
+};
+
+// Run the script.
+console.log(sentenceBuilder());
+
